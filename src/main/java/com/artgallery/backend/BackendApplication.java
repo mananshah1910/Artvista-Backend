cfa6b2baf1ded34543706306b10325d62330b58a
mananshah1910 <2400090137@kluniversity.in>
2026-04-30 18:31:06 +0530

    fix: Resolve Spring Boot local boot failure and ensure DB environment variables are used

--- src/main/java/com/artgallery/backend/BackendApplication.java
+++ src/main/java/com/artgallery/backend/BackendApplication.java
@@ -1,14 +1,21 @@
 package com.artgallery.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.io.File;
 
 @SpringBootApplication
 @EnableAsync
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		if (new File(".env").exists()) {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		}
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
